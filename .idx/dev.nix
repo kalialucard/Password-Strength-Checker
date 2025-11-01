f18ea6dbb92bdd610e52a92b5fa31094d2f4a366
sam dien <sxyn929@gmail.com>
2025-11-01 10:16:16 +0000

    Your commit message

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,8 +5,10 @@
   channel = "stable-24.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
+  
     pkgs.nodejs_20
     pkgs.zulu
+    pkgs.gh
   ];
   # Sets environment variables in the workspace
   env = {};
